959b588ed091ba125c2783650475088a64ec6363
cj <carocean.jofers@icloud.com>
2023-07-31 10:11:11 +0800

    1.0.0

--- cj-geochat-test-iapp/cj-geochat-test-iapp-remote/src/main/java/cj/geochat/test/iapp/remote/TestMiddleResource.java
+++ cj-geochat-test-iapp/cj-geochat-test-iapp-remote/src/main/java/cj/geochat/test/iapp/remote/TestMiddleResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(contextId = "testMiddleResource", value = "cj-geochat-middle-test", url = "${app.test.feign.adapter.docker.uc.url:}")
+@FeignClient(contextId = "testMiddleResource", value = "cj-geochat-test-middle", url = "${app.test.feign.adapter.docker.uc.url:}")
 public interface TestMiddleResource extends ITestResource {
     //如果Fegin代理的是get请求，那么必须在feign微服务的请求参数带上@RequestParam 或@RequestBody
     //否则不论是否指定为method = RequestMethod.GET，都会发起POST请求
